26071dc93d5d9e95d19d43527f40f69e14850815
Smiley98 <smileysofficialcinema@gmail.com>
2025-09-26 09:32:03 -0400

    Added challenge 1 solution

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -3,29 +3,26 @@
 
 int main()
 {
+    Vector2 circlePosition = { 400.0f, 400.0f };
+    Vector2 recPosition = { 100.0f, 500.0f };
+    float speed = 100.0f;   // 100 pixels per second
+
     InitWindow(800, 800, "Game");
     SetTargetFPS(60);
 
-    Vector2 circlePosition = { 400.0f, 400.0f };
-
-    // Challenge 1:
-    // Store a position
-    // Store a speed
-
     while (!WindowShouldClose())
     {
-        // Challenge 1:
-        // Store frame time using the GetFrameTime() function
-        // Add frame time * speed to position
-        // Use position to draw a shape of your choice!
-        
-        float tt = GetTime();
+        float tt = GetTime();       // Time since program start
+        float dt = GetFrameTime();  // Time since last frame (~16.67 milliseconds if 60fps)
+        recPosition.x += speed * dt;
 
         BeginDrawing();
         ClearBackground(WHITE);
 
         DrawCircleV(circlePosition + Vector2UnitX * sinf(tt) * 50.0f, 25.0f, RED);
-        DrawCircle(100, 100, 20, BLUE);
+        DrawRectangleV(recPosition, { 60.0f, 20.0f }, ORANGE);
+
+        DrawCircle(400, 790, 10, BLUE);
         DrawRectangle(0, 780, 800, 20, GRAY);
 
         DrawText("Hello world!", 10, 10, 20, LIGHTGRAY);
