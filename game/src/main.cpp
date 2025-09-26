944e8b14f97f71aef3b36d497217ec4bf55b6891
Smiley98 <smileysofficialcinema@gmail.com>
2025-09-26 13:17:07 -0400

    Challenge 1 solution

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -3,32 +3,28 @@
 
 int main()
 {
+    Vector2 circlePosition = { 400.0f, 400.0f };
+    Vector2 recPosition{ 100.0f, 500.0f };
+    float speed = 100.0f;
+
     InitWindow(800, 800, "Game");
     SetTargetFPS(60);
 
-    Vector2 circlePosition = { GetScreenWidth() * 0.5f, GetScreenHeight() * 0.5f };
-
-    // Challenge: Using a position, speed, and time, animate a shape of your choice
-    // 1) Store a position
-    // 2) Store a speed
-
     while (!WindowShouldClose())
     {
-        float tt = GetTime();   // Time since program started
-        // 3) Store frame time
-        //float dt = Time.deltaTime; <-- figure out how to do this in raylib!
-        // (Hint: use the GetFrameTime() function)
-        // 4) Change position based on speed * frame time
+        float tt = GetTime();       // Time since program started
+        float dt = GetFrameTime();  // Time between frames (~16.67 milliseconds if 60fps)
+        recPosition.x += speed * dt;
 
         BeginDrawing();
         ClearBackground(WHITE);
 
-        // 5) Draw the shape of your choice within begin/end drawing!
-
         DrawCircleV(circlePosition + Vector2UnitX * sinf(tt) * 50.0f, 20.0f, RED);
+        DrawRectangleV(recPosition, { 80.0f, 40.0f }, ORANGE);
 
         DrawCircle(100, 100, 20.0f, BLUE);
         DrawRectangle(0, 780, 800, 20, BEIGE);
+
         DrawText("Hello world!", 10, 10, 20, LIGHTGRAY);
         EndDrawing();
     }
