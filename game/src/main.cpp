d859bb4c10a041123b67febff6cf857c41d9e09a
Smiley98 <smileysofficialcinema@gmail.com>
2025-10-03 10:10:32 -0400

    Gamestate example

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,16 +1,60 @@
 #include "raylib.h"
 #include "raymath.h"
 
+enum GameState
+{
+    PRE_GAME,
+    GAME,
+    POST_GAME
+};
+
 int main()
 {
+    GameState state = PRE_GAME;
+
     InitWindow(800, 800, "Game");
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
+        switch (state)
+        {
+        case PRE_GAME:
+            if (IsKeyPressed(KEY_ENTER))
+                state = GAME;
+            break;
+
+        case GAME:
+            // Q to quit
+            if (IsKeyPressed(KEY_Q))
+                state = POST_GAME;
+            break;
+
+        case POST_GAME:
+            // R to reset
+            if (IsKeyPressed(KEY_R))
+                state = PRE_GAME;
+            break;
+        }
+
         BeginDrawing();
-        ClearBackground(WHITE);
-        DrawText("Hello world!", 10, 10, 20, LIGHTGRAY);
+        switch (state)
+        {
+        case PRE_GAME:
+            ClearBackground(WHITE);
+            DrawText("Press ENTER to start", 400, 400, 20, GREEN);
+            break;
+
+        case GAME:
+            ClearBackground(ORANGE);
+            DrawText("Press Q to quit", 400, 400, 20, BLUE);
+            break;
+
+        case POST_GAME:
+            DrawText("Press R to reset", 400, 400, 20, PINK);
+            ClearBackground(VIOLET);
+            break;
+        }
         EndDrawing();
     }
 
